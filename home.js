10aa99d67d9e58ce5abf0eb32f892eaaacf2ff50
MatteX22 <64346361+MatteX22@users.noreply.github.com>
2026-02-06 17:18:45 +0100

    Miglioramento grafico e logico del layout
    - Ridisegnato il layout dell'applicazione.
    - Implementato una prima versione di stile grafico
    - Modificato il layout della checklist
    - Imlemnetata la checklist della bravo

---
+++ home.js
@@ -0,0 +1,14 @@
+const btnBravo = document.getElementById("btnBravo");
+const btnOrdinaria = document.getElementById("btnOrdinaria");
+
+// CAMBIA QUI il nome del file della tua checklist Bravo
+const BRAVO_PAGE = "util/bravo/bravo.html";
+
+btnBravo.addEventListener("click", () => {
+  window.location.href = BRAVO_PAGE;
+});
+
+btnOrdinaria.addEventListener("click", () => {
+  // per ora placeholder
+  alert("Schermata Oscar non ancora collegata");
+});
